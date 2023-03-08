129436d4896a29d71cc936aae66a3a4ef48e6b4a
PeraltaEdith <127307439+PeraltaEdith@users.noreply.github.com>
2023-03-08 08:42:03 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Modelo de Curriculum Vitae
+Nombre y Apellido
+Profesion
+
+Correo 
+Fecha de nacimiento
+Celular
+Direccion
+
+Experiencia laboral
+
+Estudios
+
+Habilidades
+
+Idiomas
---
+++ index.js
@@ -0,0 +1,32 @@
+
+document.getElementById("mas1").addEventListener('click', function () {
+
+    document.getElementById("experiencia2").style.display = "block";
+
+    document.getElementById("mas1").style.visibility = 'hidden';
+
+})
+
+document.getElementById("mas2").addEventListener('click', function () {
+
+    document.getElementById("estudio2").style.display = "block";
+
+    document.getElementById("mas2").style.visibility = 'hidden';
+
+})
+
+document.getElementById("ver1").addEventListener('click', function () {
+
+    document.getElementById("habilidades").style.display = "block";
+
+    document.getElementById("ver1").style.visibility = 'hidden';
+
+})
+
+document.getElementById("ver2").addEventListener('click', function () {
+
+    document.getElementById("idioma").style.display = "block";
+
+    document.getElementById("ver2").style.visibility = 'hidden';
+
+})
